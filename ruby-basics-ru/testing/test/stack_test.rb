770096a44ea9011ccdf4ba7d8eb56b0d819eccb6
@hexlet/cli <support@hexlet.io>
2023-02-28 22:58:11 +0600

    submit ruby-basics-ru/testing

--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -2,10 +2,56 @@
 
 require_relative 'test_helper'
 require_relative '../lib/stack'
+require 'minitest/power_assert'
 
 class StackTest < Minitest::Test
   # BEGIN
-  
+  def setup
+    @stack = Stack.new
+  end
+
+  def test_push_adds_element_to_stack
+    @stack.push!(1)
+    assert { @stack.size == 1 }
+  end
+
+  def test_push_multiple_elements_to_stack
+    @stack.push!(1)
+    @stack.push!(2)
+    @stack.push!(3)
+    assert { @stack.size == 3 }
+    assert { @stack.to_a == [1, 2, 3] }
+  end
+
+  def test_pop_removes_last_element_from_stack
+    @stack.push!(1)
+    @stack.push!(2)
+    @stack.push!(3)
+    assert { @stack.pop! == 3 }
+    assert { @stack.to_a == [1, 2] }
+  end
+
+  def test_pop_returns_nil_if_stack_is_empty
+    assert { @stack.pop!.nil? }
+  end
+
+  def test_clear_removes_all_elements_from_stack
+    @stack.push!(1)
+    @stack.push!(2)
+    @stack.push!(3)
+    @stack.clear!
+    assert { @stack.empty? }
+  end
+
+  def test_is_empty_returns_true_if_stack_is_empty
+    assert { @stack.empty? }
+  end
+
+  def test_is_empty_returns_false_if_stack_is_not_empty
+    @stack.push!(1)
+    refute { @stack.empty? }
+  end
+
   # END
 end
 
